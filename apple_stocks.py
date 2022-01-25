2b4ca72ce47e7c4d6748da93531b4f388224143b
Alexandria Brown <axbn2190@gmail.com>
2022-01-24 20:29:02 -0500

    add problem statement for apple stocks

--- apple_stocks.py
+++ apple_stocks.py
@@ -1,4 +1,10 @@
+# Write an efficient function that takes stock_prices and returns the best profit I could have made from one purchase and one sale of one share of Apple stock yesterday.
 
+# The indices are the time (in minutes) past trade opening time, which was 9:30am local time.
+# The values are the price (in US dollars) of one share of Apple stock at that time.
+# So if the stock cost $500 at 10:30am, that means stock_prices[60] = 500.
+
+# No "shorting"—you need to buy before you can sell. Also, you can't buy and sell in the same time step—at least 1 minute has to pass.
 
 def get_max_profit(stock_prices):
     if len(stock_prices) < 2:
