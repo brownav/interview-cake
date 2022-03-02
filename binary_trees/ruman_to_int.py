e926254fc9dccd6cefc510195da9b01b1c484df8
Alexandria Brown <axbn2190@gmail.com>
2022-03-02 15:28:58 -0500

    complete roman to int"

---
+++ binary_trees/ruman_to_int.py
@@ -0,0 +1,23 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        numerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        total = 0
+
+        i = 0
+        while i < len(s):
+            if i == len(s)-1:
+                total += numerals[s[i]]
+                return total
+            if numerals[s[i]] < numerals[s[i+1]]:
+                total += (numerals[s[i+1]] - numerals[s[i]])
+                i += 1
+            else:
+                total += numerals[s[i]]
+            i +=1
+
+        return total
+
+
+# Driver code
+solution = Solution()
+solution.romanToInt("MCMXCIV")
