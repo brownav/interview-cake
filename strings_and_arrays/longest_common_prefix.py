891bc3c132a420d4e7a1a5541cba6ac349923e47
Alexandria Brown <axbn2190@gmail.com>
2022-03-04 19:34:05 -0500

    rename file, add lcp.py

---
+++ strings_and_arrays/longest_common_prefix.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        if len(strs) == 0:
+            return ""
+
+        i = 1
+        j = 0
+        count = len(strs[0])-1
+        word = strs[0]
+        while i < len(strs):
+            while strs[i].startswith(word[0:j]):
+                j += 1
+            count = min(count, j-1)
+            j = 0
+            i += 1
+
+        if count == 0:
+            return ""
+        else:
+            return word[0 : count]
+
+# Driver Code:
+solution = Solution()
+print(solution.longestCommonPrefix(["flower","flow","flight"]))
