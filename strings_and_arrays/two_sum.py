ace26d6ac2ce2d8b582b86cc8c829f23a1ecb0e9
Alexandria Brown <axbn2190@gmail.com>
2022-03-10 19:15:15 -0500

    optimize two sum

--- strings_and_arrays/two_sum.py
+++ strings_and_arrays/two_sum.py
@@ -1,3 +1,5 @@
+from typing import List
+
 # Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 # You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.
@@ -6,17 +8,13 @@
 # Output: [0,1]
 # Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]
 
-
 # time: O(n)
 # space: O(n)
 def two_sum(nums: List[int], target: int) -> List[int]:
-    dict = {}
-    
+    dict = {} # val : index
+
     for i in range(len(nums)):
-        dict[nums[i]] = i
-    
-    for i in range(len(nums)):
-        pair = target - nums[i]
-        # pair is in dict and indices are unique
-        if pair in dict and dict[pair] != i:
-            return [i, dict[pair]]
+        diff = target - nums[i]
+        if diff in dict:
+            return [dict[diff], i]
+        dict[nums[i], i]
